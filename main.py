48d6c4f13c0e681d43f8e9457bd50391128f6c3f
sarka-stepankova <stepankov.s@seznam.cz>
2022-01-21 23:22:17 +0100

    Added GUI

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import random
-#import tkinter as tk
+import tkinter as tk
+from tkinter import messagebox
 
-# pak smazat
-def printBoard(matrix):
-    for i in range(4):
-        print(*matrix[i])
-    print('-------')
+# theme from https://flatuicolors.com
+colors = {
+    '': '#bdc3c7',
+    2: '#1abc9c',
+    4: '#16a085',
+    8: '#2ecc71',
+    16: '#27ae60',
+    32: '#3498db',
+    64: '#2980b9',
+    128: '#9b59b6',
+    256: '#8e44ad',
+    512: '#f1c40f',
+    1024: '#f39c12',
+    2048: '#e67e22'
+}
 
 def newTile(matrix):
-    # presne nevím jestli kdyz se dostanu na konec hry, tak bude potreba tohle kontrolovat nebo ne
-    # TODO: rozmyslet si funci kódu ř.12-18
     counter = 0
     for row in range(4):
         for column in range(4):
@@ -34,9 +43,6 @@ def newTile(matrix):
 def newGame():
     matrix = [[0] * 4 for _ in range(4)]
     newTile(matrix)
-
-    # pak smazat, nahradit grafickým překreslením
-    #printBoard(matrix)
     return matrix
 
 # Check whether you lost or win
@@ -56,19 +62,23 @@ def verticalMoveExists(matrix):
     return False
 
 def gameOver(matrix):
+    global root
     if any(2048 in row for row in matrix):
-        print('You win!')
-        return True
+        messagebox.showinfo('2048', 'You\'ve won!')
+        root.destroy()
     elif not any(0 in row for row in matrix) and not horizontalMoveExists(matrix) and not verticalMoveExists(matrix):
-        print('You lost!')
-        return True
-    else:
-        return False
+        messagebox.showinfo('2048', 'You\'ve lost!')
+        root.destroy()
 
 #####
 
 # noodle = row/column
 def mergeNoodle(n1,n2,n3,n4):
+    '''
+    Function that takes 4 numbers from one row/col and does a slide to n1 (with merging). 
+    '''
+    
+    # slide to n1 without merging (firstly I need nonzero numbers as close as possible to n1)
     noodle = [n1, n2, n3, n4]
     newNoodle = []
     counter = 4
@@ -79,6 +89,7 @@ def mergeNoodle(n1,n2,n3,n4):
     nulls = [0] * counter
     noodle = newNoodle + nulls
     
+    # now merging nonzero numbers (from n1)
     if noodle[0] == noodle[1]: 
         noodle[0] *= 2
         noodle[1] = 0
@@ -89,6 +100,7 @@ def mergeNoodle(n1,n2,n3,n4):
         noodle[2] *= 2
         noodle[3] = 0
 
+    # another slide to n1 (zeros may occur after merge between nonzero nums)
     newNoodle = []
     counter = 4
     for num in noodle:
@@ -101,22 +113,20 @@ def mergeNoodle(n1,n2,n3,n4):
 
 def down(matrix):
     newMatrix = [[],[],[],[]]
-    for row in range(4):
-        noodle = [mergeNoodle(matrix[3][row],matrix[2][row],matrix[1][row],matrix[0][row])][0][::-1]
+    for col in range(4):
+        noodle = [mergeNoodle(matrix[3][col],matrix[2][col],matrix[1][col],matrix[0][col])][0][::-1]
         for j in range(4):
             newMatrix[j] += [noodle[j]]
     newMatrix = newTile(newMatrix)
-    gameOver(newMatrix)
     return newMatrix
 
 def up(matrix):
     newMatrix = [[],[],[],[]]
-    for row in range(4):
-        noodle = mergeNoodle(matrix[0][row],matrix[1][row],matrix[2][row],matrix[3][row])
+    for col in range(4):
+        noodle = mergeNoodle(matrix[0][col],matrix[1][col],matrix[2][col],matrix[3][col])
         for j in range(4):
             newMatrix[j] += [noodle[j]]
     newMatrix = newTile(newMatrix)
-    gameOver(newMatrix)
     return newMatrix
 
 def left(matrix):
@@ -124,7 +134,6 @@ def left(matrix):
     for row in range(4):
         newMatrix += [mergeNoodle(matrix[row][0],matrix[row][1],matrix[row][2],matrix[row][3])]
     newMatrix = newTile(newMatrix)
-    gameOver(newMatrix)
     return newMatrix
 
 def right(matrix):
@@ -133,34 +142,51 @@ def right(matrix):
         newNoodle = [mergeNoodle(matrix[row][3],matrix[row][2],matrix[row][1],matrix[row][0])]
         newMatrix += [newNoodle[0][::-1]]
     newMatrix = newTile(newMatrix)
-    gameOver(newMatrix)
     return newMatrix
 
-matrix = newGame()
-print('Stiskni jedno z tlacitek W↑,A←,S↓,D→ pro pohyb v poli.')
-printBoard(matrix)
-
-while not gameOver(matrix):
-    smer = input('Pohyb: ').lower()
-    if smer == 'w':
-        matrix = up(matrix)
-    elif smer == 'a':
-        matrix = left(matrix)
-    elif smer == 's':
-        matrix = down(matrix)
-    elif smer == 'd':
-        matrix = right(matrix)
-    else:
-        print('Spatne tlacitko, opakuj znovu. (W↑,A←,S↓,D→)')
-
-    printBoard(matrix)
-
-
-'''matrix = [[2, 0, 16, 2], [0, 0, 16, 2], [2, 4, 0, 0], [2, 4, 16, 0]]
+def updateGUI(matrix):
+    global labels
+    for row in range(4):
+        for col in range(4):
+            num = matrix[row][col]
+            if num == 0:
+                num = ''
+            labels[row][col].configure(bg=colors[num], text=str(num))
 
 root = tk.Tk()
 root.title('2048')
-root.geometry('600x400+300+150')
-#makeGUI(root)
+root.geometry('400x370')
+root.resizable(False, False)
 
-root.mainloop()'''
+messagebox.showinfo("Info","Press -arrow keys- or -w,a,s,d keys- to play the game.")
+
+labels = [[None] * 4 for _ in range(4)]
+
+for row in range(4):
+    for col in range(4):
+        labels[row][col] = tk.Label(root, bg='#bdc3c7', text='', font='Arial 18', fg='white', width=7, height=3)
+        labels[row][col].grid(column=col, row=row, padx=2, pady=2)
+
+matrix = newGame()
+updateGUI(matrix)
+
+def move(event):
+    global matrix
+    if event.char == 'w' or event.keysym == 'Up':
+        matrix = up(matrix)
+    elif event.char == 'a' or event.keysym == 'Left':
+        matrix = left(matrix)
+    elif event.char == 's' or event.keysym == 'Down':
+        matrix = down(matrix)
+    elif event.char == 'd' or event.keysym == 'Right':
+        matrix = right(matrix)
+
+    updateGUI(matrix)
+    gameOver(matrix)
+
+    if event.keysym == 'Escape':
+        root.destroy()
+
+root.bind('<Key>', move)
+
+root.mainloop()
